9a188b274edbdd71d9e6b7caf9978107a7703c04
OKUDERA_YOJI <okudera_yoji@OKUDERA-YOJI-no-MacBook-Air.local>
2018-06-18 21:39:31 +0900

    ソリューション追加

---
+++ BlackPhoneApp/BlackPhoneApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlackPhoneApp
+//
+//  Created by OKUDERA_YOJI on 2018/06/16.
+//  Copyright © 2018年 OKUDERA_YOJI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
